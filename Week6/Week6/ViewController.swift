dba7ce17b44851d6bb769bb47d7cb8bfbed8e03e
godwish1234 <godwish_jakin@hotmail.com>
2017-04-20 12:56:44 +1000

    1st check in

---
+++ Week6/Week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by GODWISH JAKIN on 20/4/17.
+//  Copyright © 2017 GODWISH JAKIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
